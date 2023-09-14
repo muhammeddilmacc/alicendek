01bae3157b9bba4fb376fe7609592feb321fd2a9
muhammeddilmacc <muhammeddilmacc@gmail.com>
2023-09-14 21:44:14 +0300

    updating backend

--- backend/app.js
+++ backend/app.js
@@ -89,6 +89,9 @@ if (process.env.NODE_ENV === 'production') {
   // app.get('*', (req, res) =>
   //   res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
   //)
+  app.get('/', (req, res) => {
+    res.send('API is running....')
+  })
 } else {
   app.get('/', (req, res) => {
     res.send('API is running....')
